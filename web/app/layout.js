ac97e1ed640915f03fcb7930d9b86d27cd9efb3f
ArslanYM <arsalanyaqmalik@gmail.com>
2026-02-04 22:41:11 +0530

    fixing build errors

--- web/app/layout.js
+++ web/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,7 +25,6 @@ export default function RootLayout({ children }) {
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <Provider>{children}</Provider>
-          <Toaster position="top-center" />
         </body>
       </html>
     </ClerkProvider>
