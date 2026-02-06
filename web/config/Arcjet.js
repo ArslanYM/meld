d6feeef7499dff8ae9d0200126076a6471afeb90
ArslanYM <arsalanyaqmalik@gmail.com>
2026-02-06 22:12:49 +0530

    token refresh fix

--- web/config/Arcjet.js
+++ web/config/Arcjet.js
@@ -8,7 +8,7 @@ export const aj = arcjet({
       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
       characteristics: ["userId"], // track requests by a custom user ID
       refillRate: 5, // refill 5 tokens per interval
-      interval: 5, // refill every 24 hours (8650), set to 5 rn
+      interval: 8650, // refill every 24 hours (8650), set to 5 rn
       capacity: 5, // bucket maximum capacity of 10 tokens
     }),
   ],
